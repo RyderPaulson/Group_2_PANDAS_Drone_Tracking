823c0d7689cc6545a943292ee21c4ed8fcde442a
Simon Zhang <zhang.simo@northeastern.edu>
2025-09-15 19:35:35 -0400

    Added code that I used to set a tracking point on a specific pixel.

---
+++ manual_point.py
@@ -0,0 +1,104 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import torch
+import argparse
+import imageio.v3 as iio
+import numpy as np
+
+from cotracker.utils.visualizer import Visualizer
+from cotracker.predictor import CoTrackerPredictor
+# Set point in first frame
+# origin = top left
+queries = torch.tensor([
+        [0., 440., 360.],  
+    ])
+queries = queries.cuda()
+
+DEFAULT_DEVICE = (
+    "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--video_path",
+        default="./assets/apple.mp4",
+        help="path to a video",
+    )
+    parser.add_argument(
+        "--checkpoint",
+        default=None,
+        help="CoTracker model parameters",
+    )
+    parser.add_argument("--grid_size", type=int, default=10, help="Regular grid size")
+
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.video_path):
+        raise ValueError("Video file does not exist")
+
+    if args.checkpoint is not None:
+        model = CoTrackerOnlinePredictor(checkpoint=args.checkpoint)
+    else:
+        model = torch.hub.load("facebookresearch/co-tracker", "cotracker3_online")
+    model = model.to(DEFAULT_DEVICE)
+    model = model.cuda()
+    window_frames = []
+
+
+    
+    
+    def _process_step(window_frames, is_first_step, grid_size):
+        video_chunk = (
+            torch.tensor(
+                np.stack(window_frames[-model.step * 2 :]), device=DEFAULT_DEVICE
+            )
+            .float()
+            .permute(0, 3, 1, 2)[None]
+        )  # (1, T, 3, H, W)
+        return model(
+            video_chunk=video_chunk,
+            is_first_step=is_first_step,
+            grid_size=grid_size,
+            queries = queries[None]
+        )
+
+    # Iterating over video frames, processing one window at a time:
+    is_first_step = True
+    for i, frame in enumerate(
+        iio.imiter(
+            args.video_path,
+            plugin="FFMPEG",
+        )
+    ):
+        if i % model.step == 0 and i != 0:
+            pred_tracks, pred_visibility = _process_step(
+                window_frames,
+                is_first_step,
+                grid_size=args.grid_size,
+            )
+            is_first_step = False
+        window_frames.append(frame)
+    # Processing the final video frames in case video length is not a multiple of model.step
+    pred_tracks, pred_visibility = _process_step(
+        window_frames[-(i % model.step) - model.step - 1 :],
+        is_first_step,
+        grid_size=args.grid_size
+    )
+
+    print("Tracks are computed")
+
+    # save a video with predicted tracks
+    seq_name = args.video_path.split("/")[-1]
+    video = torch.tensor(np.stack(window_frames), device=DEFAULT_DEVICE).permute(
+        0, 3, 1, 2
+    )[None]
+    vis = Visualizer(save_dir="./saved_videos", pad_value=120, linewidth=3)
+    vis.visualize(
+        video, pred_tracks, pred_visibility
+    )
