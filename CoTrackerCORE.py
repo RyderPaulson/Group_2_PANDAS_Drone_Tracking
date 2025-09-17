9f07203b32f21f9cbfad2180af8c7a34ddb05c67
RyderPaulson <ryderwpaulson@gmail.com>
2025-09-17 17:41:29 -0400

    Modified CoTrackerCORE to process sliding window and fixed model erroring on first call. Current Issue: CoTracker returning None for predictions.

--- CoTrackerCORE.py
+++ CoTrackerCORE.py
@@ -3,7 +3,6 @@ import numpy as np
 import os
 
 from cotracker.predictor import CoTrackerPredictor
-from cotracker.utils.visualizer import Visualizer
 
 DEFAULT_DEVICE = (
     "cuda"
@@ -59,7 +58,7 @@ class CoTrackerCORE:
         # Frame buffer for windowed processing
         self.frame_count = 0
         self.window_size = window_size
-        self.window_frames = list()
+        self.window_frames = []
 
         # Property for most recently visualized frame
         self.most_recent_frame = None
@@ -77,36 +76,21 @@ class CoTrackerCORE:
         """
         Collect a new frame and if the frame buffer is full, process them.
         :param new_frame:
-        :return: NULL or annotated frame
+        :return [x, y] prediction location:
         """
         self.frame_count += 1
 
         self.window_frames.append(new_frame)
-
-        if (
-            self.frame_count % self.model.step == 0
-            and self.frame_count != self.model.step
-            and self.frame_count != 0
-        ):
-            try:
-                # Process set of frames
-                pred_tracks, pred_visibility = self._process_step()
-
-                self.is_first_step = False
-
-                # Visualize set of frames
-                self._visualize_frame(pred_tracks, pred_visibility)
-
-                return self.most_recent_frame
-            except Exception as e:
-                print(f"Frame Count: {self.frame_count}")
-                raise e
+        
+        if self.frame_count < self.model.step * 2:
+            # If enough frames have not been passed yet to fill the sliding window, just return the empty frame.
+            # This should only run right after the model is called for however many frames are in model step. 
+            return None, None
 
         else:
-            # Return most recent visualized frame. This will make it so that the literal frame rate of the output video
-            # stream is the same as the input one but the effective frame rate will be equivalent to fps/model.step.
-            # return self.most_recent_frame
-            pass
+            # Standard prediction loop
+            pred_tracks, pred_visibility = self._process_step()
+            return pred_tracks, pred_visibility
 
     # ----------------------- Private Methods -----------------------
 
@@ -120,29 +104,5 @@ class CoTrackerCORE:
             .float()
             .permute(0, 3, 1, 2)[None]
         )  # (1, T, 3, H, W)
-        return self.model(
-            video_chunk,
-            queries=self.query_point[None],
-        )
 
-    def _visualize_frame(self, tracks, visibility):
-        """Use the cotracker visualization suite to produce an annotated frame."""
-
-        video = (
-            torch.tensor(
-                np.stack(self.window_frames[-self.model.step * 2 :]),
-                device=DEFAULT_DEVICE,
-            )
-            .float()
-            .permute(0, 3, 1, 2)[None]
-        )  # (1, T, 3, H, W)
-
-        vis = Visualizer(
-            save_dir="./media", linewidth=6, mode="cool", tracks_leave_trace=-1
-        )
-        vis.visualize(
-            video=video,
-            tracks=tracks,
-            visibility=visibility,
-            filename=f"queries{self.frame_count}",
-        )
+        return self.model(video_chunk, is_first_step=self.is_first_step, queries=self.query_point[None])
--- tCoTrackerCORE.py
+++ tCoTrackerCORE.py
@@ -1,22 +1,81 @@
 import unittest
 import os
+import numpy as np
 import imageio.v3 as iio
+from IPython.display import Video as IPyVideo
+import torch
+
+# Import CoTracker Utilities
 from CoTrackerCORE import CoTrackerCORE
+from cotracker.utils.visualizer import Visualizer, read_video_from_path
+
+# Import GUI for selecting points from Bishoy
+from custom_point_tracker import select_points
+
+DEFAULT_DEVICE = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available() else "cpu"
+)
 
 class test_CoTrackerCORE(unittest.TestCase):
-    def test_apple_video(self):
-        query_point  = [100, 100]
-        cotracker = CoTrackerCORE(query_point)
+    def test_streamed_apple(self):
+        query_point = [100, 100]
 
         # Get video
         video_path = "./media/apple.mp4"
 
+        cotracker = CoTrackerCORE(query_point)
         if not os.path.isfile(video_path):
             self.fail("Video file does not exist")
 
+        video_frames = []
+
         for i, frame in enumerate(iio.imiter(video_path, plugin="FFMPEG")):
-            cotracker.run_tracker(frame)
+            video_frames.append(frame)
+            if i < cotracker.model.step * 2:
+                cotracker.run_tracker(frame)
+            else:
+                pred_tracks, pred_visibility = cotracker.run_tracker(frame)
 
+        video = torch.tensor(np.stack(video_frames), device=DEFAULT_DEVICE).permute(
+            0, 3, 1, 2
+        )[None]
 
-if __name__ == '__main__':
+        vis = Visualizer(save_dir="./media", pad_value=120, linewidth=3)
+
+        vis.visualize(video, pred_tracks, pred_visibility)
+
+    def track_drone_from_nuav(self):
+        # Get video
+        video_path = "./media/panning_camera_mini_drone.mp4"
+        query_point = [838, 392]
+        query_frame = 52
+
+        cotracker = CoTrackerCORE(query_point, query_frame=query_frame)
+
+        if not os.path.isfile(video_path):
+            self.fail("Video file does not exist")
+
+        video_frames = []
+
+        for i, frame in enumerate(iio.imiter(video_path, plugin="FFMPEG")):
+            video_frames.append(frame)
+            if i < cotracker.model.step * 2:
+                cotracker.run_tracker(frame)
+            else:
+                pred_tracks, pred_visibility = cotracker.run_tracker(frame)
+
+            if i > 400:
+                break
+
+        video = torch.tensor(np.stack(video_frames), device=DEFAULT_DEVICE).permute(
+            0, 3, 1, 2
+        )[None]
+
+        vis = Visualizer(save_dir="./media", pad_value=120, linewidth=3)
+
+        vis.visualize(video, pred_tracks, pred_visibility)
+
+if __name__ == "__main__":
     unittest.main()
