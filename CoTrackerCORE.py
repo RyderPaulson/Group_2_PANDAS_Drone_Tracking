cb3de4fee00711ae7d4986eb46c7988a93967392
RyderPaulson <ryderwpaulson@gmail.com>
2025-09-19 12:12:11 -0400

    Fixed CoTracker not running.

--- CoTrackerCORE.py
+++ CoTrackerCORE.py
@@ -1,6 +1,5 @@
 import torch
 import numpy as np
-import os
 
 from cotracker.predictor import CoTrackerPredictor
 
@@ -9,7 +8,6 @@ DEFAULT_DEVICE = (
     if torch.cuda.is_available()
     else "mps" if torch.backends.mps.is_available() else "cpu"
 )
-
 
 class CoTrackerCORE:
     """
@@ -20,7 +18,7 @@ class CoTrackerCORE:
     # ----------------------- Public Methods -----------------------
 
     def __init__(
-        self, query_point, query_frame=0, window_size=10, checkpoint_path=None
+        self, query_point, query_frame=0, checkpoint_path=None
     ):
         """
 
@@ -28,10 +26,6 @@ class CoTrackerCORE:
         track one point at a time.
         :param query_frame: -> The time coordinate for the query.
                                Default: 0
-        :param window_size: -> Cotracker looks at a sliding door of data, so even if one additional frame is processed
-                               at a time, it can still check against previous frames to increase the accuracy of its
-                               predictions.
-                               Default: 10
         :param checkpoint_path: -> The path for the checkpoint to load the model from.
                                    Default: None which loads a fresh model from Torch Hub.
         """
@@ -57,13 +51,12 @@ class CoTrackerCORE:
 
         # Frame buffer for windowed processing
         self.frame_count = 0
-        self.window_size = window_size
         self.window_frames = []
 
         # Property for most recently visualized frame
-        self.most_recent_frame = None
-
         self.is_first_step = True
+        self.pred_tracks = torch.tensor([0])
+        self.pred_visibility = torch.tensor([0])
 
         initialization_status = (
             f"CoTracker initialized on {DEFAULT_DEVICE}"
@@ -81,16 +74,16 @@ class CoTrackerCORE:
         self.frame_count += 1
 
         self.window_frames.append(new_frame)
-        
-        if self.frame_count < self.model.step * 2:
-            # If enough frames have not been passed yet to fill the sliding window, just return the empty frame.
-            # This should only run right after the model is called for however many frames are in model step. 
-            return None, None
 
-        else:
-            # Standard prediction loop
-            pred_tracks, pred_visibility = self._process_step()
-            return pred_tracks, pred_visibility
+        # Dump old frames. The program should hold 3x the model step frames in memory at a time.
+        if len(self.window_frames) >= self.model.step * 3:
+            self.window_frames = self.window_frames[self.model.step:]
+
+        if self.frame_count % self.model.step == 0 and self.frame_count != 0:
+            self.pred_tracks, self.pred_visibility = self._process_step()
+            self.is_first_step = False
+
+        return self.pred_tracks, self.pred_visibility
 
     # ----------------------- Private Methods -----------------------
 
@@ -105,4 +98,6 @@ class CoTrackerCORE:
             .permute(0, 3, 1, 2)[None]
         )  # (1, T, 3, H, W)
 
-        return self.model(video_chunk, is_first_step=self.is_first_step, queries=self.query_point[None])
+        # TODO Get CoTracker to track a single point rather than a grid
+        # return self.model(video_chunk, is_first_step=self.is_first_step, queries=self.query_point[None])
+        return self.model(video_chunk, is_first_step=self.is_first_step, grid_size=30)
