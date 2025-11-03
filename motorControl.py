252a3ce577884b06af450402fa8a2e30f577544d
RyderPaulson <ryderwpaulson@gmail.com>
2025-11-03 18:42:49 -0500

    control 2 servos finicky

--- motorControl.py
+++ motorControl.py
@@ -5,18 +5,14 @@ import numpy as np
 # constants
 FREQUENCY = 50 #Hz
 PERIOD_MS = 1000 / FREQUENCY
-MIN_PULSE_WIDTH_MS = 0.05 # in % duty cycle for 0°
-MAX_PULSE_WIDTH_MS = 1.6  # in % duty cycle for 180°
-# NOTE: The vertical range of the turret is 60 to 180 degrees.
-
-current_angle = 60 # start at neutral
-
+MIN_PULSE_WIDTH_MS = 0.5 # in % duty cycle for 0°
+MAX_PULSE_WIDTH_MS = 2  # in % duty cycle for 180°
 
 # --- Low Level Math Functions (Conversions/Formulas) ---
 
 # Converts angle (0-180) to pulse (MIN_PULSE_WIDTH_MS to MAX_PULSE_WIDTH_MS)
 def angle_to_pulse(angle):
-    angle = max(60, min(180, angle))    # ensure within 60-180 deg
+    angle = max(0, min(180, angle))    # ensure within 0-180 deg
     pulse_ms = MIN_PULSE_WIDTH_MS + (angle / 180.0) * (MAX_PULSE_WIDTH_MS - MIN_PULSE_WIDTH_MS)
     return pulse_ms
 
@@ -33,88 +29,90 @@ class servo:
         self.pin = pin
         self.dx = 0
         self.sum = 0
+
+    def getPin(self):
+         return self.pin
         
-    def servoMoveExp(self, pin, coord, alpha=0.5):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
-        pwm = GPIO.PWM(pin, FREQUENCY)
-        pwm.start(pulse_to_duty(angle_to_pulse(current_angle)))
-        try:
+    # alpha: how much to over/undershoot
+    # beta: scale factor of rotations 
+    def servoMoveExp(self, coord, alpha=0.5, beta = 1):
             angle = self.dx * 90 + 90
             self.sum = alpha*(self.sum + angle)
-            beta = 1
             output_angle = beta * self.sum
-            output_angle = max(60, min(180, output_angle))    #ensure within 60-180 deg
+            output_angle = max(0, min(180, output_angle))    #ensure within 0-180 deg
             self.dx = coord
-            pwm.ChangeDutyCycle(pulse_to_duty(angle_to_pulse(output_angle)))
-    
-            # update angle
+
             print(f"angle={output_angle:.2f}°")
-            time.sleep(0.1)
-        finally:
-        pwm.stop()
-        GPIO.cleanup()
+
+            return output_angle
+    
+
     # Rotate the motor to a specific angle. (immediate, no decay)
-    def setServo(self, pin, angle):
-        global current_angle
+    def setServo(self, angle):
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
-        pwm = GPIO.PWM(pin, FREQUENCY)
+        GPIO.setup(self.pin, GPIO.OUT, initial=GPIO.HIGH)
+        pwm = GPIO.PWM(self.pin, FREQUENCY)
         
         duty_cycle = pulse_to_duty(angle_to_pulse(angle))
         pwm.start(duty_cycle)
-        time.sleep(1)
+        time.sleep(3)
         pwm.stop()
         GPIO.cleanup()
-        current_angle = angle
         print("Set servo to ", angle, " deg.")
 
-# Applies exp decay function to a coordinate buffer
-# NOTE: Increase alpha if you want the servo to accelerate quickly toward the target. Decrease alpha if you want a more gradual transition.
-# def servoMoveExp(pin, dx, alpha=0.5):
-#     global current_angle
+        # Rotate the motor to a specific angle. (immediate, no decay)
+    def setServoGivenPulseMS(self, pulsems):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin, GPIO.OUT, initial=GPIO.HIGH)
+        pwm = GPIO.PWM(self.pin, FREQUENCY)
+        
+        duty_cycle = pulse_to_duty(pulsems/1000)
+        pwm.start(3)
+        time.sleep(3)
+        pwm.stop()
+        GPIO.cleanup()
+        print("Set servo to ", 45, " deg.")
 
-#     GPIO.setmode(GPIO.BOARD)
-#     GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
-#     pwm = GPIO.PWM(pin, FREQUENCY)
-#     pwm.start(pulse_to_duty(angle_to_pulse(current_angle)))
+
+def trackCoords(servoX, servoY, dx, dy):
+    pin1 = servoX.getPin()
+    pin2 = servoY.getPin()
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin1, GPIO.OUT, initial=GPIO.HIGH)
+    pwm1 = GPIO.PWM(pin1, FREQUENCY)
+    GPIO.setup(pin2, GPIO.OUT, initial=GPIO.HIGH)
+    pwm2 = GPIO.PWM(pin2, FREQUENCY)
+
+    s1Angle = servoX.servoMoveExp(dx)
+    s2Angle = servoY.servoMoveExp(dy)
+
+    pwm1.ChangeDutyCycle(pulse_to_duty(angle_to_pulse(s1Angle)))
+    pwm2.ChangeDutyCycle(pulse_to_duty(angle_to_pulse(s2Angle)))
+
+    time.sleep(.5)
     
-#     try:
-#         target_angle = current_angle + dx
-#         print("Target angle: ", target_angle, "°")
+    pwm1.stop()
+    pwm2.stop()
+    GPIO.cleanup()
 
-#         for step in range(steps):
-#             # This uses the exponetial decay formula: 1 - e^(-kt)
-#             #   Here, -k is alpha and t is the time, or here, proportion of step until finished
-#             fraction = (1 - np.exp(-alpha * ((step + 1) / steps)))  # amount to change per step
-            
-#             current_angle += (target_angle - current_angle) * fraction
+cameraServo = servo(32)
+baseServo = servo(33)
 
-#             current_angle = max(60, min(180, current_angle))    #ensure within 60-180 deg
-            
-#             # ypdate angle
-#             pwm.ChangeDutyCycle(pulse_to_duty(angle_to_pulse(current_angle)))
+# cameraServo.setServoGivenPulseMS(400)
+# baseServo.setServoGivenPulseMS(400)
+# baseServo.setServo(0)
 
-#             print(f"Step {step+1}/{steps} → angle={current_angle:.2f}°")
-#             time.sleep(0.1)
+for i in range(100):
+    trackCoords(baseServo, cameraServo, 1, 1)
 
-#         pwm.ChangeDutyCycle(pulse_to_duty(angle_to_pulse(target_angle))) #ensures exact angle always reached
 
-#     finally:
-#         pwm.stop()
-#         GPIO.cleanup()
-
-# setServo(32,60)     # set angle to 60 degrees
-# servoMoveExp(32, 120)   # the camera should now be 180 degrees (60+120)
-# setServo(32,180)
-
-servo = servo(32)
-servo.servoMoveExp(32, 1)
-servo.servoMoveExp(32, .8)
-servo.servoMoveExp(32, .2)
-servo.servoMoveExp(32, .001)
-servo.servoMoveExp(32, -1)
-servo.servoMoveExp(32, -.8)
-servo.servoMoveExp(32, -.2)
-servo.servoMoveExp(32, 0)
-servo.servoMoveExp(32, 0)
+# servo.servoMoveExp(32, 1)
+# servo.servoMoveExp(32, .8)
+# servo.servoMoveExp(32, .2)
+# servo.servoMoveExp(32, .001)
+# servo.servoMoveExp(32, -1)
+# servo.servoMoveExp(32, -.8)
+# servo.servoMoveExp(32, -.2)
+# servo.servoMoveExp(32, 0)
+# servo.servoMoveExp(32, 0)
